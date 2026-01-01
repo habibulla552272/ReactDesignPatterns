f6deeaa9597e62832a2739d68d883e949b28cb16
Md.Habibulla <mdhabibulla146425@gmail.com>
2026-01-01 23:53:54 +0600

    add component

---
+++ container-presenter-pattern/src/presenter-component/userProfileComponent.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const userProfileComponent = () => {
+  return (
+    <div>userProfileComponent</div>
+  )
+}
+
+export default userProfileComponent
---
+++ container-presenter-pattern/src/presenter-component/userProfilePresenter.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const userProfilePresenter = () => {
+  return (
+    <div>userProfilePresenter</div>
+  )
+}
+
+export default userProfilePresenter
